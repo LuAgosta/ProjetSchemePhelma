c4dfaa8184585dfc777efb4ddf798e4fd9ac5362
loulaloulalou <lu.agosta@gmail.com>
2016-10-16 22:38:09 +0200

    tests_step2

---
+++ scheme/01_test.scm
@@ -0,0 +1,7 @@
+; TEST_RETURN_CODE=PASS	
+; TEST_COMMENT=test 	
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+( abc )
+
+
---
+++ scheme/02_if.scm
@@ -0,0 +1,8 @@
+; TEST_RETURN_CODE=PASS	
+; TEST_COMMENT=if  	
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(if (< 4 5) "oh yeah" "oh no" ) 
+
+
