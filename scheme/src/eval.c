75b8f94b6390426928035c6630b6fe65b5ed0964
NOEPG <paul.gauthier.funkys@gmail.com>
2016-10-16 23:43:23 +0200

    Ajout de la fonction in_lenv()

--- scheme/src/eval.c
+++ scheme/src/eval.c
@@ -24,6 +24,37 @@ void addvar(object var, object val){
 	lenv->this.pair.car = make_pair(make_pair(var,val),o);
 }
 
+/*Recherche d'une variable dans environnements*/
+object in_lenv(object var){
+	object k = lenv;
+	object l;
+	while(k->this.pair.cdr != nil){
+		l = k->this.pair.car;
+		while(l->this.pair.cdr != nil){
+			if(strcmp(caar(l)->this.string,var->this.string)==0){
+				return(cdar(l));
+			}
+			l = l->this.pair.cdr;
+		}
+		if(strcmp(caar(l)->this.string,var->this.string)==0){
+			return(cdar(l));
+		}
+		k=k->this.pair.cdr;
+	}
+	l = k->this.pair.car;
+	while(l->this.pair.cdr != nil){
+		if(strcmp(caar(l)->this.string,var->this.string)==0){
+			return(cdar(l));
+		}
+		l = l->this.pair.cdr;
+	}
+	if(strcmp(caar(l)->this.string,var->this.string)==0){
+		return(cdar(l));
+	}
+	return nil;		/*il faut afficher un msg d'erreur du type Unbound Variable*/
+}
+
+
 object sfs_eval( object input ) {
 /**auto-evaluants**/ 
 	if ((input->type != SFS_SYMBOL) && (input->type != SFS_PAIR) && (input->type != SFS_NIL)) {
