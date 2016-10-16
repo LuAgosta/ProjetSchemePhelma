6b949ef9fb7c0a47fef62c8425dc6bbfe50deb8e
loulaloulalou <lu.agosta@gmail.com>
2016-10-16 21:06:46 +0200

    auto-évaluants

--- scheme/src/eval.c
+++ scheme/src/eval.c
@@ -19,12 +19,10 @@ int is_form (char* name, object input ) {
 
 object sfs_eval( object input ) {
 /**auto-evaluants**/ 
-	if (input -> type != ( SFS_SYMBOL && SFS_PAIR ) ) {
+	if ((input -> type !=  SFS_SYMBOL ) &&  (input ->type != SFS_PAIR)  ) {
 		return input ; 
 	}
-	if (input -> type == SFS_PAIR && input -> this.pair.car -> type != ( SFS_SYMBOL && SFS_PAIR ) ) {
-		return input -> this.pair.car ;
-	}
+
 	
 /* quote */ 
 
