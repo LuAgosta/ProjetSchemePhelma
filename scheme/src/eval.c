8ac5f86b8c00d55f687966714cfa8feac8dac943
NOEPG <paul.gauthier.funkys@gmail.com>
2016-10-24 16:17:15 +0200

    Update eval.c

--- scheme/src/eval.c
+++ scheme/src/eval.c
@@ -19,41 +19,6 @@ int is_form (char* name, object input ) {
 	return 0 ; 
 }
 
-/*Ajouter une variable et sa valeur dans l'environnement courant*/
-void addvar(object var, object val){
-	object o = lenv->this.pair.car;
-	lenv->this.pair.car = make_pair(make_pair(var,val),o);
-}
-
-/*Recherche d'une variable dans environnements*/
-object in_lenv(object var){
-	object k = lenv;
-	object l;
-	while(k->this.pair.cdr != nil){
-		l = k->this.pair.car;
-		while(l->this.pair.cdr != nil){
-			if(strcmp(caar(l)->this.string,var->this.string)==0){
-				return(cdar(l));
-			}
-			l = l->this.pair.cdr;
-		}
-		if(strcmp(caar(l)->this.string,var->this.string)==0){
-			return(cdar(l));
-		}
-		k=k->this.pair.cdr;
-	}
-	l = k->this.pair.car;
-	while(l->this.pair.cdr != nil){
-		if(strcmp(caar(l)->this.string,var->this.string)==0){
-			return(cdar(l));
-		}
-		l = l->this.pair.cdr;
-	}
-	if(strcmp(caar(l)->this.string,var->this.string)==0){
-		return(cdar(l));
-	}
-	return nil;		/*il faut afficher un msg d'erreur du type Unbound Variable*/
-}
 
 /*Modifier un object*/
 void modify_object(object tomod, object mod) {
