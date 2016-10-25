1054e169181f390b69aea0a8d632dc5de0c0a835
loulaloulalou <lu.agosta@gmail.com>
2016-10-25 11:31:51 +0200

    14_and

---
+++ scheme/tests_step2/14_and.scm
@@ -0,0 +1,9 @@
+; TEST_RETURN_CODE=PASS
+; TEST_COMMENT=Test define evolved
+;
+
+
+(and (< 4 5) (> 4 5))
+(and (= "o" "o") (< 4 7 ))
+(and (> 4 5) (< 4 5)) 
+(and (< 4 5 ) (< 5 6 ) (< 6 7) (< 7 8) (< 8 9))
