9cdfddad6b68e25647f2e1d57eb7861922cf0891
loulaloulalou <lu.agosta@gmail.com>
2016-11-12 17:10:57 +0100

    Ajout de prim.h

---
+++ scheme/include/prim.h
@@ -0,0 +1,30 @@
+
+/**
+ * @file prim.h
+ * @author Agosta Lucie and Noé Paul-Gauthier>
+ * @date Sat Nov 12 16:39:11 2016
+ * 
+ */
+
+#ifndef _PRIM_H_
+#define _PRIM_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+
+#include "object.h"
+
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PRIM_H_ */
