d6b5fe2a4423e508ac08d1fef87ce15d0c72371f
NOEPG <paul.gauthier.funkys@gmail.com>
2016-11-24 00:52:51 +0100

    Create list.scm

---
+++ scheme/tests_step3/list.scm
@@ -0,0 +1,9 @@
+; TEST_RETURN_CODE=PASS
+; TEST_COMMENT=Test de la primitive list
+;
+(list "miles" "davis" 1926)
+(define where "alton")
+(list where 1926)
+(list (+ 4 5) 7)
+(list '(4 5) 78)
+(list (list 7 8 9) 7 8 9)
