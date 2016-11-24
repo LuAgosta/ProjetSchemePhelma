7638a6ec0e6aaab9b62e533a10e40a640e43728b
NOEPG <paul.gauthier.funkys@gmail.com>
2016-11-24 18:22:59 +0100

    Create moins.scm

---
+++ scheme/tests_step3/moins.scm
@@ -0,0 +1,14 @@
+; TEST_RETURN_CODE=PASS
+; TEST_COMMENT=Test de la primitive -
+;
+(- 4 5)
+(- 5 4)
+(- 4 1 2 3)
+(- (- 4 5) 4)
+(- '4 7)
+(- 4 '7)
+(define a 1)
+(define b 22)
+(- a b)
+'(- 4 5)
+(- 1)
