3ca6971af1ff341f5ae814a94197ee773daecb63
NOEPG <paul.gauthier.funkys@gmail.com>
2016-11-24 18:24:24 +0100

    Create plus.scm

---
+++ scheme/tests_step3/plus.scm
@@ -0,0 +1,14 @@
+; TEST_RETURN_CODE=PASS
+; TEST_COMMENT=Test de la primitive +
+;
+(+ 1 44)
+(+ 1 44 2 3)
+(+ (+ 4 5) 10)
+(+ '4 '7 4)
+(+ 4 '1)
+(define a 1)
+(define b 22)
+(+ a b)
+'(+ 7 8)
+(+ 4)
+(+ 4 -6)
