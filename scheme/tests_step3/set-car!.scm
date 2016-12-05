9e6054e0effefc3e5345ac7c52d60ea509d9e337
NOEPG <paul.gauthier.funkys@gmail.com>
2016-12-05 14:44:58 +0100

    Update set-car!.scm

--- scheme/tests_step3/set-car!.scm
+++ scheme/tests_step3/set-car!.scm
@@ -1,13 +1,12 @@
 ; TEST_RETURN_CODE=PASS
 ; TEST_COMMENT= set-car!
 ;
-(set-car! (cons 1 (cons 2())) 77)
-(set-car! (cons (+ 4 6) (cons "toto" ())) "titi")
-(set-car! '(#\a 2) #\0)
 (define a '(4 5 6))
+a
 (set-car! a 58)
 a
-(set-car! (quote (() 5) ) 8); on regarde si () est modifié
-() 
+(define b '(() 5))
+(set-car! b 8) ;on regarde si () est modifié
+()
 
 
