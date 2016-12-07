99f16c5e599d65a5b7ad8d1a15e8f07d13e4cdc5
NOEPG <paul.gauthier.funkys@gmail.com>
2016-12-07 14:57:59 +0100

    Update env.h

--- scheme/include/env.h
+++ scheme/include/env.h
@@ -8,6 +8,8 @@ extern "C" {
 #include "object.h"
 
 void addvar(object var, object val);
+void addvarenv(object var, object val, object env);
+object addenv(void);
 object in_envc(object var);
 object in_lenv(object var);
 
