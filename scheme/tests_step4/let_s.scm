21bcce291dd61b389bcb05c24a144026c712f8d1
NOEPG <paul.gauthier.funkys@gmail.com>
2016-12-12 23:42:47 +0100

    Create let_s.scm

---
+++ scheme/tests_step4/let_s.scm
@@ -0,0 +1,10 @@
+; TEST_RETURN_CODE=PASS
+; TEST_COMMENT= let*
+;
+(let ((x 2) (y 3)) (let* ((x 7) (z (+ x y))) (* z x)))
+(let ((x 2) (y 3)) (let* ((x 7) (z (+ x y))) (let* ((a z) (b x)) (* a b))))
+(define x 45)
+(define y "hi!")
+(let ((x 2) (y 3)) (let* ((x 7) (z (+ x y))) (* z x)))
+x
+y
