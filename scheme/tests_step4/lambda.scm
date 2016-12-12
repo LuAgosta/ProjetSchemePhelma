3c336a17849614b8ad5baac719f7ee7013128138
loulaloulalou <lu.agosta@gmail.com>
2016-12-12 20:26:25 +0100

    begin.res lambda.res lambda.scm

---
+++ scheme/tests_step4/lambda.scm
@@ -0,0 +1,13 @@
+; TEST_RETURN_CODE=PASS
+; TEST_COMMENT= lambda
+;
+(lambda (x) (+ x x))
+(lambda (x y) (+ x y))
+((lambda (x) (+ x x)) 6)
+((lambda (x y) ( + x y )) 9 7) 
+((lambda (x y z) (set! x 2) (set! y 9) (set! z 10) (* x y z )) 1 1 1)
+(define x 5)
+((lambda (x) (set! x 8) x ) x)  
+x
+(define carre (lambda  (x) (* x x)))
+(carre 2)
