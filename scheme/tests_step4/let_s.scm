cffd5973cebdf165564a80886790c0bb10d19033
NOEPG <paul.gauthier.funkys@gmail.com>
2016-12-13 10:32:33 +0100

    Update let_s.scm

--- scheme/tests_step4/let_s.scm
+++ scheme/tests_step4/let_s.scm
@@ -1,6 +1,7 @@
 ; TEST_RETURN_CODE=PASS
 ; TEST_COMMENT= let*
 ;
+let*
 (let ((x 2) (y 3)) (let* ((x 7) (z (+ x y))) (* z x)))
 (let ((x 2) (y 3)) (let* ((x 7) (z (+ x y))) (let* ((a z) (b x)) (* a b))))
 (define x 45)
