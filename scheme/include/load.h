f8e34fe8344527dc5779860e65143d9fd02a8810
NOEPG <paul.gauthier.funkys@gmail.com>
2016-12-14 13:44:50 +0100

    Create load.h

---
+++ scheme/include/load.h
@@ -0,0 +1,16 @@
+#ifndef _LOAD_H_
+#define _LOAD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "object.h"
+
+object load(char* );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LOAD_H_ */
